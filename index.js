a9d3d6873d46d5a2425d3dbca11ddf2a03c43887
Michał Ryciak <michal.ryciak@agora.pl>
2017-03-23 11:19:27 +0100

    middleware
    Change-Id: I0480c98e878dc5f3165a2d8852c189435df0fffd

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.end('hello world');
-});
-server.listen(3000, function () {
-  console.log("When this callback is invoked our server is listening on port: " + 3000);
-});
+var express = require('express')
+var app = express()
+
+function logRequest(req, res, next) {
+  console.log('incoming request at ', new Date());
+  next();
+};
+
+app.use(logRequest);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
